7dfc862bad6e440a8d786c596d66f41802faa1c8
akashkuchankar <kuchankarakash@gmail.com>
2022-10-06 20:33:14 -0700

    Add project files.

---
+++ print the pattarn/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_the_pattarn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i = 1; i <=5; i++)              //for 5*5 table
+            //{
+            //    for(int j = 1; j <= 5; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //for (int i = 1; i <= 5; i++)        // ******
+            //{                                     //****  
+            //    for(int j = 5; j >=i; j--)       // ***  
+            //    {                                 //**  
+            //        Console.Write("*");           //*  
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 1; i <= 5; i++)              //*
+            //{                                         //**            
+            //    for (int j = 1; j <= i; j++)          //***
+            //    {                                     //****
+            //        Console.Write("*");               //*****    
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for(int i = 1; i <=5; i++)                  //1
+            //{                                           //12
+            //    for(int j = 1; j <=i; j++)              //123     
+            //    {                                       //1234
+            //        Console.Write(j);                   //12345    
+
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+
+    }
+}
